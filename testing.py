664393f9f4a63fb505c4cb22e9785db7cac183bd
shakeel2211 <shakeelkamoka2277@gmail.com>
2024-11-17 21:28:16 -0800

    lecture no 8 assignment operator increment or decrement method

--- testing.py
+++ testing.py
@@ -1,6 +1,9 @@
-#  Lecture no 7 Arithmetic Operators in python
-    # Arithmetic operators
-print("Addition:", 5 + 3)            #  Addition mean jma karna
-print("Subtraction:", 5 - 3)         #  Subtraction mean manfi karna
-print("Multiplication:", 5 * 3)      #  Multiplication mean 5 ko 3 say zerb dena
-print("Division:", 10 / 3)           #  Division mean 10 ko 3 py taqseem karna
+
+  # lecture no 8 assignment operator increment or decrement method
+x = 10
+x + = 5       # x = x + 5  mean x ki value main 5 jma karna
+print(x)
+x = 10
+X = x - 5       # x = x - 5   mean x ki value main say 5 manfi karna
+print(x)    
+
