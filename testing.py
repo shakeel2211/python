61e0bf166305c9cde71d55490d2b2149b7f34265
shakeel2211 <shakeelkamoka2277@gmail.com>
2024-11-17 22:31:59 -0800

    Lecture no 10 Logical Operators in python

--- testing.py
+++ testing.py
@@ -1,11 +1,10 @@
-  # Lecture no 9 Comparison Operators in python
+ # Lecture no 10 Logical Operators in python
 x = 10
 y = 20
-print(x==y)   # x equal hai y k ( false )
-
-
-print(x!=y)   # x equal nahi hai y k ( true )
-print(x>y)    # x bara hai y k  ( false )
-print(x<y)    # x chota hai y k  ( true )
-print(x>=y) # x bara hai yaa braber hai y k(false)
-print(x<=y)# x chorta hai yaa braber hai y k(true)
+print(x==10 and x<y) # Logical and main  2no condition theak hon to true aye gaa
+x = 10
+y = 20
+print(x==10 or x<y or x==y) # Logical or main koi ak codition true ho to ans true aye gaa.
+x = 10
+y = 20
+print(not x!=10 or x<y or x==y) # logical not main koi ak condition true hai to true aye ager sari condition false hain to false aye gaa
