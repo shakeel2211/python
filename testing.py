bcde3edfa96aad8cd5ca076528e50686411dfd54
shakeel2211 <shakeelkamoka2277@gmail.com>
2024-11-17 21:46:15 -0800

    Lecture no 9 Comparison Operators in python

--- testing.py
+++ testing.py
@@ -1,9 +1,11 @@
-
-  # lecture no 8 assignment operator increment or decrement method
+  # Lecture no 9 Comparison Operators in python
 x = 10
-x + = 5       # x = x + 5  mean x ki value main 5 jma karna
-print(x)
-x = 10
-X = x - 5       # x = x - 5   mean x ki value main say 5 manfi karna
-print(x)    
+y = 20
+print(x==y)   # x equal hai y k ( false )
 
+
+print(x!=y)   # x equal nahi hai y k ( true )
+print(x>y)    # x bara hai y k  ( false )
+print(x<y)    # x chota hai y k  ( true )
+print(x>=y) # x bara hai yaa braber hai y k(false)
+print(x<=y)# x chorta hai yaa braber hai y k(true)
