b478e617dac3dae31cacbc55b58b8dd393b61a5b
shakeel2211 <shakeelkamoka2277@gmail.com>
2024-11-17 21:12:14 -0800

    Lecture no 7 Arithmetic Operators in python

--- testing.py
+++ testing.py
@@ -1,4 +1,6 @@
-# Lec no 6 concatenate two sting
-a = "hello"
-b = "shakeel"
-print(a + " " + b)
+#  Lecture no 7 Arithmetic Operators in python
+    # Arithmetic operators
+print("Addition:", 5 + 3)            #  Addition mean jma karna
+print("Subtraction:", 5 - 3)         #  Subtraction mean manfi karna
+print("Multiplication:", 5 * 3)      #  Multiplication mean 5 ko 3 say zerb dena
+print("Division:", 10 / 3)           #  Division mean 10 ko 3 py taqseem karna
