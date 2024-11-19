f4c59222866dc9900e4375d57c4019028cb8f85f
shakeel2211 <shakeelkamoka2277@gmail.com>
2024-11-18 17:10:48 -0800

    Lecture no 14 Data type in python

--- testing.py
+++ testing.py
@@ -1,5 +1,37 @@
-# lecture no 12 identify operator in python
-x = 10
-y = 10
-print(x is y, x==y) # yee to value print kar skty hain x is y mean x braber hai y k and x==y mean x braber hai y k 
-print(x is not y , x!=y) # x is not y mean x braber nahi hai y k or  X!=y mean x braber nahi hai y k
+
+# Lecture no 14 Data type in python
+a=5
+print(a,type(a))  # int
+
+
+a=5.5
+print(a,type(a))   # float
+
+
+a= 2+5j
+print(a,type(a))   # complex
+
+
+a= "hello"
+print(a,type(a))   # string
+
+
+a= [10,20,30,"ws"]
+print(a , type(a))   #  list
+
+
+a=(19,20,"hello")
+print(a, type(a))    #  tuple
+
+
+d={
+   "course_name" : "python" ,      # directory
+   "course_duration" : "2 Month"  
+   }
+print(d["course_name"])
+print(d["course_duration"])
+print(d,type(d))
+
+
+s= { 20,40,50,60}
+print(s,type(s))    # set
