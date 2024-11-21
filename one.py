75f6fcbfa2ffbd9cd8c6736d4f57c896847add6a
shakeel2211 <shakeelkamoka2277@gmail.com>
2024-11-21 15:30:06 -0800

    Lecture no 16 how to make calculator in python

--- one.py
+++ one.py
@@ -1,6 +1,20 @@
-#  lecture no 16 conditional statement in python
-# if statement no1    (user value 20)
-a = int(input("Enter the value:-")) # user say ak input lay gay.ose int main convert kare gay.
-if a % 2 == 0:  # if a mean jo user say input wali value leni hai wo or os ka % 2==0 mean is ka answer 0 k braber hai to yee condition true ho gee. 
-    print(a, "Even Namber")  # print kar dy gay k a even number hai.
-    
+   # simple calculator 
+print('''     
+      + Add
+      - Subtrack
+      * Multiply
+      / Divide
+      ''') # calculator k liye yee sab print kare gay
+num1 = int(input("Enter the value1:-")) # input ki value 1 aye gee
+num2 = int(input("Enter the value2:-")) # input ki value 2 aye gee
+opr = input("Enter the Opr..(+ , - , * , / )") # ye input lay gay simble bnane k liye
+if opr == "+":  # if opr ka simble == + k braber hai to yee print aye gaa.
+    print(num1+num2)
+elif opr == "-": # if opr ka simble == - ka braber hai to yee print aye gaa.
+    print(num1 - num2)
+elif opr == "*":
+    print(num1 * num2)
+elif opr == "/":
+    print(num1 / num2)
+else:       # else mean ager kuch bhi nahi to invalid opr.. print aye gaa
+    print("invalid opr...")
