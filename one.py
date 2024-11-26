31b6dd1f996331550ddcc527a5328206a513885d
shakeel2211 <shakeelkamoka2277@gmail.com>
2024-11-25 20:07:21 -0800

    lecture no 18 for loop in python range

--- one.py
+++ one.py
@@ -1,20 +1,12 @@
-   # simple calculator 
-print('''     
-      + Add
-      - Subtrack
-      * Multiply
-      / Divide
-      ''') # calculator k liye yee sab print kare gay
-num1 = int(input("Enter the value1:-")) # input ki value 1 aye gee
-num2 = int(input("Enter the value2:-")) # input ki value 2 aye gee
-opr = input("Enter the Opr..(+ , - , * , / )") # ye input lay gay simble bnane k liye
-if opr == "+":  # if opr ka simble == + k braber hai to yee print aye gaa.
-    print(num1+num2)
-elif opr == "-": # if opr ka simble == - ka braber hai to yee print aye gaa.
-    print(num1 - num2)
-elif opr == "*":
-    print(num1 * num2)
-elif opr == "/":
-    print(num1 / num2)
-else:       # else mean ager kuch bhi nahi to invalid opr.. print aye gaa
-    print("invalid opr...")
+ # Range in python  (example no 1)
+ # range( start , end , step)
+ 
+ for a in range(5):
+    print(a)
+    print()
+for a in range(2,6):
+    print(a)
+    print()
+for a in range(1,10,2):
+    print(a)
+
