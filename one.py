123a2f9dcb081a0f601dd0b4adfbc46d6f1e22c4
shakeel2211 <shakeelkamoka2277@gmail.com>
2024-11-26 18:44:54 -0800

    lecture no 19 while loop in python

--- one.py
+++ one.py
@@ -1,12 +1,14 @@
- # Range in python  (example no 1)(for loop)
- # range( start , end , step)
  
- for a in range(5):
-    print(a)
-    print()
-for a in range(2,6):
-    print(a)
-    print()
-for a in range(1,10,2):
-    print(a)
-
+   # lecture no 19 while loop in python
+  # example no1
+i = 1
+while i <= 10:  # شرط کو صحیح لکھیں
+    print(i,"k")  # انڈینٹ کو درست رکھیں
+    i = i + 1  # i کی ویلیو کو انکریمنٹ کریں
+    print(i)
+    
+    # example no2
+    a = 10  # a ki value 10 hai
+    while a >=1:  # a bara hai 10 say to condition true hai.
+        print(a,"hello")
+        a = a-1
