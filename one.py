be55ae03c5171dfc92036b38d8efa2f08241fd99
shakeel2211 <shakeelkamoka2277@gmail.com>
2024-11-26 19:18:59 -0800

    lecture no 20 what is a string index and string slicing in python

--- one.py
+++ one.py
@@ -1,14 +1,12 @@
  
-   # lecture no 19 while loop in python
-  # example no1
-i = 1
-while i <= 10:  # شرط کو صحیح لکھیں
-    print(i,"k")  # انڈینٹ کو درست رکھیں
-    i = i + 1  # i کی ویلیو کو انکریمنٹ کریں
-    print(i)
-    
-    # example no2
-    a = 10  # a ki value 10 hai
-    while a >=1:  # a bara hai 10 say to condition true hai.
-        print(a,"hello")
-        a = a-1
+# what is a string index and string slicing in python
+
+s="welcome to shakeel kamoka"
+print(s[12])  # string index left to right 
+
+print(s[-13])  # string index left to right
+
+# string slice 
+
+s="welcome to shakeel kamoka"
+print(s[0:18:2])
