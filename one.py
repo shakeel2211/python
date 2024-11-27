ef3d7b5f1e17c6e62263df96a38f6645a8f904f3
shakeel2211 <shakeelkamoka2277@gmail.com>
2024-11-26 20:40:18 -0800

    lec no 21 string iteration in python

--- one.py
+++ one.py
@@ -1,12 +1,8 @@
- 
-# what is a string index and string slicing in python
 
-s="welcome to shakeel kamoka"
-print(s[12])  # string index left to right 
-
-print(s[-13])  # string index left to right
-
-# string slice 
-
-s="welcome to shakeel kamoka"
-print(s[0:18:2])
+# lec no 21 string iteration in python
+s = "welcome to shakeel kamoka" # koi bhi string lay gay
+a = len(s) # string ki length nikalye gay to os k liye len()function lay gay.
+print(a)  # print a kare gay total length chak karne k liye.
+for e in range(a): # for loop use kare gay a lay gay length k liye
+    print(s[e])  # print main s aye gaa kue k s ki value lane hai or pher [e] aye gaa. kue k index k tera kaam kare gaa. 
+    
