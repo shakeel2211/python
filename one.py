55cef2cfad5afbd8350d9ecc6bb52ed5c73918e6
shakeel2211 <shakeelkamoka2277@gmail.com>
2024-11-28 20:34:21 -0800

    lec no 27 What is List Iteration in Python

--- one.py
+++ one.py
@@ -1,5 +1,13 @@
- # lecture no 26 How to Create List in Python
-p=[10,20,30,40,50,"Hello"] # list main ak yaa ak say zayeda value get karni ho to .
-print(p[3],p[5])   # p[3] say index ki value ka pta chly gaa 30 hai agar ap nee hello say add karna hai to agay ,p[4] add kar dy.
-print(p[0:2])   # is ka mean k value 0 say start ho or 2 tak chaly
-print(p[0::2])  # is ka mean k value 0 say start ho or pori read ho or pher 2 ka increment ho.
+# lec no 27 What is List Iteration in Python.
+
+u = [10,20,30,40,50,60,70,80,90] # sab say phely hum list lay gay.
+v = len(u)    # length get kare gay
+for a in range(v):  # for loop use kare gay.
+    print(u[a])     # print kare gay.
+    
+    
+    # for loop ko reverse kase karna hai.
+u = [10,20,30,40,50,60,70,80,90] # sab say phely hum list lay gay.
+v = len(u)    # length get kare gay
+for a in range(v-1,-1,-1):
+    print(u[a])
