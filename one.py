2e51de02e7287a81c9b595e3cfa9c64a8c7e647d
shakeel2211 <shakeelkamoka2277@gmail.com>
2024-11-28 18:43:07 -0800

    lecture no 26 How to Create List in Python

--- one.py
+++ one.py
@@ -1,5 +1,5 @@
-
-# lecture no 25 what is a string format in python
-
-w = "welcome {a:>10} to {b} shakeel kamoka" .format(a=30 , b="hello");
-print(w)
+ # lecture no 26 How to Create List in Python
+p=[10,20,30,40,50,"Hello"] # list main ak yaa ak say zayeda value get karni ho to .
+print(p[3],p[5])   # p[3] say index ki value ka pta chly gaa 30 hai agar ap nee hello say add karna hai to agay ,p[4] add kar dy.
+print(p[0:2])   # is ka mean k value 0 say start ho or 2 tak chaly
+print(p[0::2])  # is ka mean k value 0 say start ho or pori read ho or pher 2 ka increment ho.
