a09eb2cb5fdf0c8059bd1f50662178fa44e1208d
shakeel2211 <shakeelkamoka2277@gmail.com>
2024-12-04 03:53:29 -0800

    lecture no 28 list comprehension in python

--- one.py
+++ one.py
@@ -1,13 +1,15 @@
-# lec no 27 What is List Iteration in Python.
+# lecture no 28 list comprehension in python.
 
-u = [10,20,30,40,50,60,70,80,90] # sab say phely hum list lay gay.
-v = len(u)    # length get kare gay
-for a in range(v):  # for loop use kare gay.
-    print(u[a])     # print kare gay.
+p = []
+for a in range(1,101): # for loop ka use 
+    p.append(a)    # normal formula append() function say
+    
+    print(p)
     
     
-    # for loop ko reverse kase karna hai.
-u = [10,20,30,40,50,60,70,80,90] # sab say phely hum list lay gay.
-v = len(u)    # length get kare gay
-for a in range(v-1,-1,-1):
-    print(u[a])
+    a = [ b for b in  range(1,101) if b%2==0 ] # for loop ka use hoga or pher filter ka formula use ho gaa  (if b%2==0) .or wo formula niklne gaa jo 2 py divide ho gaa.
+    print(a) # is main list comprehension ka kiya hai.
+    
+    s = "hello kamoka" # list comprehension example no 2.
+    t = [u for u in s]
+    print(t)
